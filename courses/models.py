bdb019d0600ac79c86e6d12b25fcc52159a28439
HallWoodZhang <hallwood_zhang@163.com>
2018-02-25 10:58:14 +0800

    fix some bug and add oganization app

--- courses/models.py
+++ courses/models.py
@@ -11,7 +11,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u'课程名称')
     desc = models.CharField(max_length=300, verbose_name=u'课程描述')
     detail = models.TextField(verbose_name=u'课程详情')
-    degree = models.CharField(max_length=10, choice=(('primary', u'初级'), ('middle', u'中级'), ('senior', u'高级')))
+    degree = models.CharField(max_length=10, choices=(('primary', u'初级'), ('middle', u'中级'), ('senior', u'高级')))
     learn_time = models.IntegerField(default=0, verbose_name=u'学习时长（分钟）')
     students_nums = models.IntegerField(default=0, verbose_name=u'学习人数')
     fav_nums = models.IntegerField(default=0, verbose_name=u'收藏人数')
@@ -25,7 +25,7 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
-    course = models.ForeignKey(Course, verbose_name=u'课程')
+    course = models.ForeignKey(Course, verbose_name=u'课程', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, verbose_name=u'章节名')
     add_time = models.DateTimeField(verbose_name=u'添加时间', default=datetime.now)
 
@@ -35,7 +35,7 @@ class Lesson(models.Model):
 
 
 class Video(models.Model):
-    lesson = models.ForeignKey(Lesson, verbose_name=u'章节')
+    lesson = models.ForeignKey(Lesson, verbose_name=u'章节', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, verbose_name=u'视频名')
     add_time = models.DateTimeField(verbose_name=u'添加时间', default=datetime.now)
 
@@ -45,7 +45,7 @@ class Video(models.Model):
 
 
 class CourseResource(models.Model):
-    course = models.ForeignKey(Course, verbose_name=u'课程')
+    course = models.ForeignKey(Course, verbose_name=u'课程', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, verbose_name=u'资源名')
     download = models.FileField(max_length=100, upload_to='course/resource/%Y/%m', verbose_name=u'资源文件')
     add_time = models.DateTimeField(verbose_name=u'添加时间', default=datetime.now)
